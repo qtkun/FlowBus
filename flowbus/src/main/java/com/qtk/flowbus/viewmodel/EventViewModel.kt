d9ef212471b74b6e2e547eba542f28946d6fa8c5
qtkun <qutkun@gmail.com>
2023-06-12 13:53:18 +0800

    fisrt commit

---
+++ flowbus/src/main/java/com/qtk/flowbus/viewmodel/EventViewModel.kt
@@ -0,0 +1,106 @@
+package com.qtk.flowbus.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.whenStateAtLeast
+import com.qtk.flowbus.observe.OnReceived
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import java.util.concurrent.ConcurrentHashMap
+
+class EventViewModel: ViewModel() {
+    private val eventFlows = ConcurrentHashMap<String, MutableSharedFlow<Any>>()
+    private val stickyEventFlows = ConcurrentHashMap<String, MutableSharedFlow<Any>>()
+
+    private fun getEventFlow(name: String, isSticky: Boolean): MutableSharedFlow<Any> {
+        return if (isSticky) {
+            stickyEventFlows[name]
+                ?: MutableSharedFlow<Any>(1, 1, BufferOverflow.DROP_OLDEST).also {
+                stickyEventFlows[name] = it
+            }
+        } else {
+            eventFlows[name] ?: MutableSharedFlow<Any>(0).also {
+                eventFlows[name] = it
+            }
+        }
+    }
+
+    fun postEventDelay(
+        eventName: String,
+        value: Any,
+        delayTimeMillis: Long
+    ) {
+        listOf(
+            getEventFlow(eventName, true),
+            getEventFlow(eventName, false)
+        ).forEach { eventFlow ->
+            viewModelScope.launch {
+                delay(delayTimeMillis)
+                eventFlow.emit(value)
+            }
+        }
+    }
+
+    fun <T>observeEvent(
+        lifecycleOwner: LifecycleOwner,
+        minActiveState: Lifecycle.State,
+        dispatcher: CoroutineDispatcher,
+        eventName: String,
+        isSticky: Boolean,
+        onReceived: OnReceived<T>
+    ) {
+        lifecycleOwner.lifecycleScope.launch {
+            lifecycleOwner.lifecycle.whenStateAtLeast(minActiveState) {
+                getEventFlow(eventName, isSticky)
+                    .distinctUntilChanged()
+                    .collect {
+                        launch(dispatcher) {
+                            invokeReceived(it, onReceived)
+                        }
+                    }
+            }
+        }
+    }
+
+    fun <T> observeWithoutLifecycle(
+        eventName: String,
+        isSticky: Boolean,
+        onReceived: OnReceived<T>
+    ) = viewModelScope.launch {
+        getEventFlow(eventName, isSticky)
+            .distinctUntilChanged()
+            .collect { value ->
+                invokeReceived(value, onReceived)
+            }
+    }
+
+    fun removeStickEvent(eventName: String) {
+        stickyEventFlows.remove(eventName)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun clearStickEvent(eventName: String) {
+        stickyEventFlows[eventName]?.resetReplayCache()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Any> invokeReceived(value: Any, onReceived: OnReceived<T>) {
+        try {
+            onReceived.invoke(value as T)
+        } catch (e: ClassCastException) {
+            Log.w("FlowBus","class cast error on message received: $value")
+        } catch (e: Exception) {
+            Log.w("FlowBus", "error on message received: $value")
+        }
+    }
+
+}
